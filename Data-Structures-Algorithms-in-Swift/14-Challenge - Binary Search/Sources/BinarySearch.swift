bb0338937d71cc01e8205330aea03487c1d8853e
王兴彬_Binboy <binboy@live.com>
2020-05-02 22:28:33 +0800

    Update

--- Data-Structures-Algorithms-in-Swift/14-Challenge - Binary Search/Sources/BinarySearch.swift
+++ Data-Structures-Algorithms-in-Swift/14-Challenge - Binary Search/Sources/BinarySearch.swift
@@ -28,63 +28,63 @@
 
 
 func findIndices(of value: Int, in array: [Int]) -> CountableRange<Int>? {
-  guard let startIndex = startIndex(of: value, in: array, range: 0..<array.count)
-  else {
-    return nil
-  }
-  guard let endIndex = endIndex(of: value, in: array, range: 0..<array.count)
-  else {
-    return nil
-  }
-  return startIndex..<endIndex
+    guard let startIndex = startIndex(of: value, in: array, range: 0..<array.count)
+        else {
+            return nil
+    }
+    guard let endIndex = endIndex(of: value, in: array, range: 0..<array.count)
+        else {
+            return nil
+    }
+    return startIndex..<endIndex
 }
 
 func startIndex(of value: Int, in array: [Int], range: CountableRange<Int>) -> Int? {
-  let middleIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-  
-  if middleIndex == 0 || middleIndex == array.count - 1 {
+    let middleIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+
+    if middleIndex == 0 || middleIndex == array.count - 1 {
+        if array[middleIndex] == value {
+            return middleIndex
+        } else {
+            return nil
+        }
+    }
+
     if array[middleIndex] == value {
-      return middleIndex
+        if array[middleIndex - 1] != value {
+            return middleIndex
+        } else {
+            return startIndex(of: value, in: array, range: range.lowerBound..<middleIndex)
+        }
+    } else if value < array[middleIndex] {
+        return startIndex(of: value, in: array, range: range.lowerBound..<middleIndex)
     } else {
-      return nil
+        return startIndex(of: value, in: array, range: middleIndex..<range.upperBound)
     }
-  }
-  
-  if array[middleIndex] == value {
-    if array[middleIndex - 1] != value {
-      return middleIndex
-    } else {
-      return startIndex(of: value, in: array, range: range.lowerBound..<middleIndex)
-    }
-  } else if value < array[middleIndex] {
-    return startIndex(of: value, in: array, range: range.lowerBound..<middleIndex)
-  } else {
-    return startIndex(of: value, in: array, range: middleIndex..<range.upperBound)
-  }
 }
 
 func endIndex(of value: Int, in array: [Int], range: CountableRange<Int>) -> Int? {
-  let middleIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
-  
-  if middleIndex == 0 || middleIndex == array.count - 1 {
+    let middleIndex = range.lowerBound + (range.upperBound - range.lowerBound) / 2
+
+    if middleIndex == 0 || middleIndex == array.count - 1 {
+        if array[middleIndex] == value {
+            return middleIndex + 1
+        } else {
+            return nil
+        }
+    }
+
     if array[middleIndex] == value {
-      return middleIndex + 1
+        if array[middleIndex + 1] != value {
+            return middleIndex + 1
+        } else {
+            return endIndex(of: value, in: array, range: middleIndex..<range.upperBound)
+        }
+    } else if value < array[middleIndex] {
+        return endIndex(of: value, in: array, range: range.lowerBound..<middleIndex)
     } else {
-      return nil
+        return endIndex(of: value, in: array, range: middleIndex..<range.upperBound)
     }
-  }
-  
-  if array[middleIndex] == value {
-    if array[middleIndex + 1] != value {
-      return middleIndex + 1
-    } else {
-      return endIndex(of: value, in: array, range: middleIndex..<range.upperBound)
-    }
-  } else if value < array[middleIndex] {
-    return endIndex(of: value, in: array, range: range.lowerBound..<middleIndex)
-  } else {
-    return endIndex(of: value, in: array, range: middleIndex..<range.upperBound)
-  }
 }
 
 
@@ -94,21 +94,21 @@ func endIndex(of value: Int, in array: [Int], range: CountableRange<Int>) -> Int
 
 
 extension RandomAccessCollection where Element: Comparable {
-  func binarySearch(for value: Element, in range: Range<Index>? = nil) -> Index? {
-    let range = range ?? startIndex..<endIndex
+    func binarySearch(for value: Element, in range: Range<Index>? = nil) -> Index? {
+        let range = range ?? startIndex..<endIndex
 
-    guard range.lowerBound < range.upperBound else {
-      return nil
-    }
+        guard range.lowerBound < range.upperBound else {
+            return nil
+        }
 
-    let size = distance(from: range.lowerBound, to: range.upperBound)
-    let middle = index(range.lowerBound, offsetBy: size / 2)
-    if self[middle] == value {
-      return middle
-    } else if self[middle] > value {
-      return binarySearch(for: value, in: range.lowerBound..<middle)
-    } else {
-      return binarySearch(for: value, in: index(after: middle)..<range.upperBound)
+        let size = distance(from: range.lowerBound, to: range.upperBound)
+        let middle = index(range.lowerBound, offsetBy: size / 2)
+        if self[middle] == value {
+            return middle
+        } else if self[middle] > value {
+            return binarySearch(for: value, in: range.lowerBound..<middle)
+        } else {
+            return binarySearch(for: value, in: index(after: middle)..<range.upperBound)
+        }
     }
-  }
 }
